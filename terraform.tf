e6381056be525a5aadc47c7b985c906a5fc90df1
dinara21 <dmcmoore21@gmail.com>
2022-06-29 23:29:58 +0000

    added vpc

---
+++ terraform.tf
@@ -0,0 +1,5 @@
+terraform {
+  required_version = ">=1.1.1"
+}
+
+# some companies hard code versions
---
+++ variable.tf
@@ -0,0 +1,52 @@
+variable "cidr_block" {
+  description = "Please provide CIDR block for VPC"
+  type        = string
+  default     = ""
+}
+variable "region" {
+  description = "Please provide a region name"
+  type        = string
+  default     = ""
+}
+variable "tags" {
+  description = "Please provide a tag for recources"
+  type        = map(any)
+  default     = {}
+
+}
+variable "public_subnet1" {
+  description = "Please provide CIDR block"
+  type        = string
+  default     = ""
+
+}
+variable "public_subnet2" {
+  description = "Please provide CIDR block"
+  type        = string
+  default     = ""
+
+}
+variable "public_subnet3" {
+  description = "Please provide CIDR block"
+  type        = string
+  default     = ""
+
+}
+variable "private_subnet1" {
+  description = "Please provide CIDR block"
+  type        = string
+  default     = ""
+
+}
+variable "private_subnet2" {
+  description = "Please provide CIDR block"
+  type        = string
+  default     = ""
+
+}
+variable "private_subnet3" {
+  description = "Please provide CIDR block"
+  type        = string
+  default     = ""
+
+}
